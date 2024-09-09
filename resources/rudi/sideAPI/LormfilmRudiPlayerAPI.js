c6d6a93a336d67031e375c5bd1d0789d8e90f283
queinu <zzedovec@yahoo.com>
2024-09-10 00:08:17 +0300

    Update LormfilmRudiPlayerAPI.js

--- resources/rudi/sideAPI/LormfilmRudiPlayerAPI.js
+++ resources/rudi/sideAPI/LormfilmRudiPlayerAPI.js
@@ -1,11 +1,11 @@
-const player = document.querySelector('#oframeplayer-'+document.querySelector('body > div').id.substring(7)+' > hdvbplayer > video');
+const playerObj = document.querySelector('#oframeplayer-'+document.querySelector('body > div').id.substring(7)+' > hdvbplayer > video');
 
 function play()
 {
-    player.play();
+    playerObj.play();
 }
 
 function pause()
 {
-    player.pause();
+    playerObj.pause();
 }
