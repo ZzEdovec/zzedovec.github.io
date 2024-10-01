acfa87730208062130ea384781be3d651ca3f59b
queinu <zzedovec@yahoo.com>
2024-10-01 18:09:35 +0300

    Update Rudi PlayerJS injectors + self PlayerJS

---
+++ resources/rudi/PlayerJS2Rudi.js
@@ -0,0 +1,37 @@
+function play()
+{
+    playerObj.api('play');
+}
+
+function pause()
+{
+    playerObj.api('pause');
+}
+
+function seekTo(seconds)
+{
+    playerObj.api('seek',seconds);
+}
+
+function getTime()
+{
+    return playerObj.api('time');
+}
+
+function getDuration()
+{
+    return playerObj.api('duration');
+}
+
+function setVolume(volume)
+{
+    playerObj.api('volume',volume);
+}
+
+function getPlayerState()
+{
+    if (playerObj.api('playing'))
+        return 1;
+    else
+        return 2;
+}
